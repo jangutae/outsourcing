8b8b82b9ed11590bb1f0bb6aacb51cade1b044b5
jut <jangutm600.gmail.com>
2024-12-07 19:54:45 +0900

    시연 영상 하면 끝이다!!!!

--- src/main/java/com/example/outsourcing/review/controller/ReviewController.java
+++ src/main/java/com/example/outsourcing/review/controller/ReviewController.java
@@ -5,16 +5,12 @@ import com.example.outsourcing.review.dto.ReviewResponseDto;
 import com.example.outsourcing.review.service.ReviewService;
 import com.example.outsourcing.user.entity.User;
 import com.example.outsourcing.user.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.Lint;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -48,23 +44,4 @@ public class ReviewController {
 
         return ResponseEntity.ok().body(allReviewByStoreId);
     }
-
-//    @GetMapping("/{storeId}")
-//    public ResponseEntity<List<ReviewResponseDto>> readAllReview(
-//            @PathVariable Long storeId,
-//            @RequestParam(required = false, value = "star") List<Integer> star,
-//            @SessionAttribute(value = "id") User user
-//    ) {
-//        List<ReviewResponseDto> responseDtos = new ArrayList<>();
-//
-//        if (star != null) {
-//            // 파라미터 값이(별점이) 있을 경우
-//            responseDtos = reviewService.readByStar(storeId, userId, star.get(0), star.get(1));
-//        } else {
-//            //파라미터 값이 없을 경우 (별점없이 가게로만 조회할 때)
-//            responseDtos = reviewService.readAllReview(storeId, userId);
-//        }
-//        return new ResponseEntity<>(responseDtos, HttpStatus.OK);
-//    }
-
 }
